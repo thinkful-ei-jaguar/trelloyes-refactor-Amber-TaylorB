2d451dcdfc1b641dae40b1ef823b0ae6bba988e1
Taylor Bradshaw <taylordyoung1@gmail.com>
2020-01-03 19:03:37 -0500

    fixed bug

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import STORE from './STORE';
 import List from './List'
 import './App.css';
 
@@ -11,43 +12,12 @@ const newRandomCard = () => {
     content: 'lorem ipsum',
   }
 }
-/*function omit(obj, keyToOmit) {
-  return Object.entries(obj).reduce(
-    (newObj, [key, value]) =>
-        key === keyToOmit ? newObj : {...newObj, [key]: value},
-    {}
-  );
-}*/
+
 class App extends Component {
-  state = this.props;
-  //const omittedCard = omit(cardId, allCards);
-  handleAdd = (id) => {
-    const cardyCard = newRandomCard();
-    console.log(cardyCard.id);
-    const currentList = this.state.store.lists.map(list => {
-      if (list.id === id) {
-        return {
-          ...list,
-          cardIds: [...list.cardIds, cardyCard.id]
-        };
-      }
-        return list;
-    })
+  state = {
+    store: STORE
+  };
 
-
-    this.setState({
-      store: {
-        lists: currentList,
-        allcards: {
-          ...this.state.store.allCards,
-          [cardyCard.id]: cardyCard,  
-      }
-    }
-  })
-    console.log(this.state)
-
-  }
-  
   handleDelete = (cardId) => {
     const updatedLists = this.state.store.lists.map(list => {
       list.cardIds = list.cardIds.filter(id => id !== cardId);
@@ -57,13 +27,40 @@ class App extends Component {
     this.setState({
         store : {
           lists: updatedLists,
-          allCards: this.state.store.allCards,
+          allCards: this.state.store.allCards
       }})
   }
 
+  handleAdd = (id) => {
+    const cardyCard = newRandomCard();
+
+    const currentList = this.state.store.lists.map(list => {
+      if (list.id === id) {
+        return {
+          ...list,
+          cardIds: [...list.cardIds, cardyCard.id]
+        };
+      }
+        return list;
+    });
+
+
+    this.setState({
+      store: {
+        lists: currentList,
+        allCards: {
+          ...this.state.store.allCards,
+          [cardyCard.id]: cardyCard
+        }
+      }
+    })
+
+  }
+  
+
+
   render() {
     const { store } = this.state;
-    console.log(store);
     return (
       <main className='App'>
         <header className='App-header'>
